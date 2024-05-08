38bda90589ad32df843e55e4950dbcb09d1c4412
Victor Souza <rochadesouzavictor@gmail.com>
2024-05-07 21:49:13 -0300

    Adição arquivos aula10

---
+++ aulas/aula10/app.js
@@ -0,0 +1,25 @@
+// npx express-generator --no-view aula10
+// npm install 
+// npm install swagger-ui-express yaml dotenv mongoose
+// npm install --save-dev nodemon supertest jest 
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const routerProdutos = require('./routes/router_produtos');
+
+var app = express();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/produtos', routerProdutos);
+
+module.exports = app;
